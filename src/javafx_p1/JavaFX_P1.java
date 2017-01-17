bff5c442e1a361713772f8ed637054843e597ae0
Guiller <Guiller@192.168.1.119>
2017-01-17 12:09:47 +0100

    Prueba de mod para git hub de javaFX_P1.java en linea 23.

--- src/javafx_p1/JavaFX_P1.java
+++ src/javafx_p1/JavaFX_P1.java
@@ -20,7 +20,7 @@ public class JavaFX_P1 extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+        //Prueba de git nº 1
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
